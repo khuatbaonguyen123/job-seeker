14cba2f3aaee7ab3f2a1cf8d68bac5eb177b20b4
TrMiDuc <124168615+TrMiDuc@users.noreply.github.com>
2024-11-29 00:19:10 +0700

    up

---
+++ job-seeker/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { Footer } from './component/footer';
+import { Header } from './component/header';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './component/css/style.css';
+import './component/css/responsive.css';
+import './component/css/colors/colors.css';
+import 'animate.css'
+import 'line-awesome/dist/line-awesome/css/line-awesome.min.css';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
